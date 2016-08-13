befa7290bed8cd3d0de7af3cd7c34e1a4d6122bc
xzysun <xzysun@gmail.com>
2016-08-13 22:11:05 +0800

    调整Demo

--- GridTable/ViewController.m
+++ GridTable/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "GTTableView.h"
 
-@interface ViewController ()
+@interface ViewController () <GTTableViewDelegate>
 
 @property (nonatomic, strong) GTTableView *tableView;
 @end
@@ -23,6 +23,15 @@
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.tableView = [[GTTableView alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)-64.0)];
     [self.view addSubview:self.tableView];
+    self.tableView.delegate = self;
+    GTTableInfo *info = [GTTableInfo tableInfoWithRows:30 Columns:20];
+    info.rowInofs.firstObject.isRowFixed = YES;
+    info.columnInfos.firstObject.isColumnFixed = YES;
+    [info buildTableCellInfoWithBlock:^(GTTableCellInfo *cellInfo, NSInteger row, NSInteger column) {
+        //
+    }];
+    self.tableView.tableInfo = info;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,4 +39,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - TableView Delegate
+-(void)tableView:(GTTableView *)tableView didSelectCellAtRow:(NSInteger)row Column:(NSInteger)column
+{
+    NSLog(@"tableview didSelectCell:%lu-%lu", row, column);
+}
 @end
